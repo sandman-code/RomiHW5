a8d985a8ed43517605cbc29868d61ad310f639f1
Caden Crist <cade3217@gmail.com>
2021-03-05 00:26:22 -0500

    final changes

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@ Romi32U4ButtonB pb;
 Romi32U4Motors motors;
 
 Bluemotor motor;
-long reading = 0;
+long oldReading = 0;
+long newReading = 0;
 long old = 0;
 unsigned long time = 5;
 float newt = 0;
+long freq = 0;
+
 void setup()
 {
   Serial.begin(9600);
@@ -20,14 +23,22 @@ void loop()
 {
   if (pb.isPressed())
   {
+    newt = millis() / 1000;
+    newReading = motor.getPosition();
+    freq = (newReading - oldReading) / (newt - old);
     unsigned long endTime = millis() + 100;
-    motor.setEffort(200); // effort test value
+    motor.setEffort(400); // effort test value
     Serial.println(motor.getPosition());
+    Serial.println(millis());
+    Serial.println((freq * 60) / 540);
+    Serial.println();
     while (millis() < endTime)
       ;
   }
   else
   {
+    old = millis() / 1000;
+    oldReading = motor.getPosition();
     motor.setEffort(0);
     motor.reset();
   }
